796d1251ebfe9a1be73cd1c9f52897f2b733d814
Asima Sabat <asima.sabat17@gmail.com>
2022-12-11 06:06:27 +0530

    Add Student class builder changes

--- src/main/java/builder/Student.java
+++ src/main/java/builder/Student.java
@@ -1,5 +1,14 @@
 package builder;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
 public class Student {
-    
+    String fname;
+    String lname;
+    String email;
+    String phone;
+    String address;
 }
--- src/test/java/buildertestpckg/ValidateBuilder.java
+++ src/test/java/buildertestpckg/ValidateBuilder.java
@@ -3,6 +3,7 @@ package buildertestpckg;
 import builder.Database;
 import builder.DatabaseType;
 import builder.FaultyDatabase;
+import builder.Student;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -45,5 +46,16 @@ public class ValidateBuilder {
         Assert.assertEquals("compare host : ","localhost", Database.properties().getHost());
         Assert.assertEquals("If mysql is set, on fetching it should be the same", DatabaseType.MYSQL, Database.properties().getType());
         Assert.assertEquals("If name is set, on fetching it should be the same", "testdb", Database.properties().getDatabaseName());
+    }
+
+    @Test
+    public void testStudentBuilder(){
+        Student student = Student.builder().
+                fname("Asima").
+                lname("Sabat").
+                build();
+        Assert.assertNotNull("If build method is called, student object should not be null", student);
+        Assert.assertEquals("compare fname : ","Asima", student.getFname());
+        Assert.assertEquals("compare lname : ","Sabat", student.getLname());
     }
 }
